25bad263c82704a43a9c6e4b5cbdf095356b8706
Daniel W. Hieber <dwhieb@gmail.com>
2016-01-27 19:55:37 -0800

    collapsing menu on mobile layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,32 @@
 const main = {
 
-  resize () {
+  menuButton: document.getElementById('menuButton'),
+  nav: document.querySelector('#nav ul'),
 
-    console.log('ran');
+  resize: function () {
 
     const mobile = window.matchMedia && window.matchMedia('(max-device-width: 699px)').matches || screen.width < 700 || window.innerWidth < 700;
 
     if (mobile) {
       document.body.classList.add('mobile');
       document.body.classList.remove('desktop');
+      main.menuButton.style.display = 'flex';
+      main.nav.style.display = 'none';
     } else {
       document.body.classList.add('desktop');
       document.body.classList.remove('mobile');
+      main.menuButton.style.display = 'none';
+      main.nav.style.display = 'flex';
     }
+  },
+
+  toggle (el) {
+    el.style.display = getComputedStyle(el).display === 'flex' ? 'none' : 'flex';
   }
 
 };
 
 main.resize();
 
+main.menuButton.addEventListener('click', () => main.toggle(main.nav));
 window.addEventListener('resize', throttle(main.resize));
