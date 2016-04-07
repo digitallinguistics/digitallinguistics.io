71b9a1a8d4b921f59813c35b90e76cd1d341fc9a
Daniel W. Hieber <dwhieb@gmail.com>
2016-04-07 10:03:08 -0700

    refactor app.js

--- app.js
+++ app.js
@@ -2,57 +2,61 @@
 require('./lib/config');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const credentials = require('./lib/credentials');
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
+const handlers = require('./lib/handlers');
 const http = require('http');
-const middleware = require('./lib/middleware');
+const path = require('path');
+
+const hbsOptions = {
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: {
+    // don't use arrow functions here - need to retain value of `this`
+    section: function section (name, opts) {
+      if (!this.sections) { this.sections = {}; }
+      this.sections[name] = opts.fn(this);
+      return null;
+    }
+  }
+};
 
 const app = express(); // initialize Express app
-const handlebars = expressHandlebars.create(middleware.hbsOptions); // initialize Handlebars
+const handlebars = expressHandlebars.create(hbsOptions); // initialize Handlebars
 
 // app settings
 app.disable('x-powered-by'); // hide server information in the response
 app.enable('trust proxy'); // trust the Azure proxy server
-app.engine('handlebars', handlebars.engine); // declare Handlebars engine
-app.set('port', process.env.PORT || 3000); // set local port to 3000
-app.set('view engine', 'handlebars'); // use Handlebars for templating
+app.engine('.hbs', handlebars.engine); // declare Handlebars engine
+app.set('port', process.env.PORT); // set port for the app (3000 on localhost)
+app.set('view engine', '.hbs'); // use Handlebars for templating
 
 // middleware
-app.use(middleware.logUrl); // URL logging for debugging
-app.use('/static', cors(), express.static(__dirname + '/public')); // enable CORS and routing for static files
-
-// TODO: change references to all static files in DLx projects to use `/static` rather than just `/`
-// deprecate the following line
-app.use(express.static(__dirname + '/public')); // routing for static files
-
-app.use(cookieParser(credentials.secret)); // cooking handling
-app.use(middleware.requestParser); // pre-formats header, body, and query
-app.use(bodyParser.json()); // parse JSON data in the request body
+app.use(express.static(path.join(__dirname, '/public'))); // routing for static files
 app.use(bodyParser.urlencoded({ extended: false })); // parse form data in the request body
+app.use(cookieParser(process.env.COOKIE_SECRET)); // cooking handling
+
+// URL logging for debugging
+app.use((req, res, next) => {
+  console.log(`Requested URL: ${req.url}`);
+  next();
+});
 
 // routing
 require('./lib/router')(app);
 
 // catch-all error handlers
-app.use(middleware.error404);
-app.use(middleware.error500);
+app.use(handlers.error404);
+app.use(handlers.error500);
 
 // start server
 const server = http.createServer(app);
 
 server.listen(app.get('port'), () => {
   console.log(`Server started. Press Ctrl+C to terminate.
-    Project: dlx-org
-    Port: ${app.get('port')}
-    Time: ${new Date()}
-    Node: ${process.version}
-    Env: ${global.env}`);
+  Project: dlx-org
+  Port: ${app.get('port')}
+  Time: ${new Date()}
+  Node: ${process.version}
+  Env: ${process.env.NODE_ENV}`);
 });
-
-// dev modules
-if (global.env === 'local') {
-  require('./lib/dev');
-  require('./build/build');
-}
