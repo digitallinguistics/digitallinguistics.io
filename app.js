146ec2c67a90ec5f2139dfe309bbd0f24a79489b
Daniel W. Hieber <dwhieb@gmail.com>
2016-04-11 03:04:38 -0700

    ready to create content for developer subdomain

--- app.js
+++ app.js
@@ -5,28 +5,16 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const http = require('http');
-const package = require('./package');
+const middleware = require('./lib/middleware');
 const path = require('path');
 const router = require('./lib/router');
 const users = require('./lib/users');
 const vhost = require('vhost');
 
-const hbsOptions = {
-  defaultLayout: 'main',
-  extname: '.hbs',
-  helpers: {
-    // don't use arrow functions here - need to retain value of `this`
-    section: function section (name, opts) {
-      if (!this.sections) { this.sections = {}; }
-      this.sections[name] = opts.fn(this);
-      return null;
-    }
-  }
-};
-
+/* eslint-disable new-cap */
 const app = express(); // initialize main Express app
-const dev = express.Router(); /* create the router for the `development.digitallinguistics.org` subdomain */ // eslint-disable-line
-const handlebars = expressHandlebars.create(hbsOptions); // initialize Handlebars
+const dev = express.Router(); // create the router for the `development.digitallinguistics.org` subdomain
+const handlebars = expressHandlebars.create(middleware.hbsOptions); // initialize Handlebars
 
 // app settings
 app.disable('x-powered-by'); // hide server information in the response
@@ -39,47 +27,22 @@ app.set('view engine', '.hbs'); // use Handlebars for templating
 app.use(express.static(path.join(__dirname, '/public'))); // routing for static files
 app.use(bodyParser.urlencoded({ extended: false })); // parse form data in the request body
 app.use(cookieParser(process.env.COOKIE_SECRET)); // cooking handling
+app.use(middleware.logger); // URL logging for debugging
+app.use(middleware.locals); // inject local variables for Handlebars templates
+app.use(users); // user management
 app.use(vhost(`developer.${process.env.DOMAIN}`, dev)); // bind the `developer` subdomain to the dev router
-
-// URL logging for debugging
-// inject global variables for Handlebars templates
-app.use((req, res, next) => {
-  'use strict';
-  console.log(`Requested URL: ${req.method} ${req.url}`);
-  let domain = process.env.DOMAIN;
-  if (domain === 'localhost') { domain += `:${process.env.PORT}`; }
-  res.locals.cdn = 'http://digitallinguistics.blob.core.windows.net';
-  res.locals.baseUrl = `//${domain}`;
-  res.locals.domain = domain;
-  next();
-});
-
-// user management
-app.use(users);
+dev.use(middleware.developer); // set Handlebars layout to `dev`
 
 // routing
 router.main(app);
 router.developer(dev);
-
 if (process.env.NODE_ENV === 'localhost') {
   router.test(app);
 }
 
 // catch-all error handlers
-app.use((req, res, next) => { // eslint-disable-line
-  res.render('error', {
-    status: 404,
-    error: 'Not found'
-  });
-});
-
-app.use((req, res, next) => { // eslint-disable-line
-  res.render('error', {
-    status: 500,
-    error: 'Server error',
-    details: `Internal server error. Please consider opening an issue on GitHub: ${package.bugs}`
-  });
-});
+app.use(middleware.error404);
+app.use(middleware.error500);
 
 // start server
 const server = http.createServer(app);
