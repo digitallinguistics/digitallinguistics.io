183a3584adb80cdac70bd1420f908458ead40cbc
dwhieb <dwhieb@gmail.com>
2018-12-01 15:53:04 -0600

    home and blog routes

--- app/middleware/router.js
+++ app/middleware/router.js
@@ -2,4 +2,19 @@ const Router = require(`koa-router`);
 
 const router = new Router();
 
+const get      = router.get.bind(router);
+const redirect = router.redirect.bind(router);
+
+get(`/`, context => {
+  context.body = `Digital Linguistics (DLx)`;
+});
+
+// NOTE: router.redirect doesn't work with URLs
+get(`/blog`, context => {
+  context.status = 301;
+  context.redirect(`https://medium.com/digital-linguistics`);
+});
+
+redirect(`/home`, `/`);
+
 module.exports = router.routes();
