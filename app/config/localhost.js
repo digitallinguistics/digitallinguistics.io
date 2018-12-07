09fb5b8dea4bc60b3ceb6f196ee3bdd47716738f
dwhieb <dwhieb@gmail.com>
2018-12-07 16:07:21 -0600

    bypass CDN during builds

--- app/config/localhost.js
+++ app/config/localhost.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CDN:             `https://storage.digitallinguistics.io`,
+  CDN:             `https://digitallinguistics.blob.core.windows.net`,
   LOG_APP_ERRORS:  true,
   LOG_REQUESTS:    true,
   LOG_USER_ERRORS: true,
