4122bac0bfe3d02fe58ba32b79737ff6f87f4d15
dwhieb <dwhieb@gmail.com>
2018-12-15 23:10:43 -0600

    style introduction to Bibliography page

--- app/middleware/handlebars.js
+++ app/middleware/handlebars.js
@@ -1,12 +1,16 @@
 const handlebars  = require(`handlebars`);
+const helpers     = require(`handlebars-helpers`);
 const path        = require(`path`);
 const viewEngine  = require(`koa-hbs`);
+
+const { length } = helpers.array();
 
 // Options for view engine
 const options = {
   defaultLayout: `main/main`,
   extname:       `.hbs`,
   handlebars,
+  helpers:       { length },
   layoutsPath:   path.join(__dirname, `../../views/layouts`),
   partialsPath:  path.join(__dirname, `../../views/components`),
   viewPath:      path.join(__dirname, `../../views/pages`),
