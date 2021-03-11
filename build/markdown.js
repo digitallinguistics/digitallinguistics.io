6985f937293f8a140d64efd920fbc864946d521b
Daniel W. Hieber <dwhieb@gmail.com>
2021-03-10 21:36:08 -0600

    purge repo

--- build/markdown.js
+++
@@ -1,9 +0,0 @@
-import Markdown from 'markdown-it';
-
-export default new Markdown({
-  breaks:      true,
-  html:        true,
-  linkify:     true,
-  quotes:      `“”‘’`,
-  typographer: true,
-});
